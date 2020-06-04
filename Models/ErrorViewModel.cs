1e42a5150819635a1ca311064f2c76699c2088d3
Patrick Segantine <patricksegantine@gmail.com>
2020-06-04 10:01:06 -0300

    Commit inicial

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace balta_aspnetcore_deploy_githubactions.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
